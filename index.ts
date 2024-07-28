25b06159793d304af967d28246ca8339b2180c45
Victor Stepanov <vix522430@bk.ru>
2024-07-28 19:38:50 +0300

    feat: развернул проект

---
+++ index.ts
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+
+import { buildApp, AppOptions } from './app';
+
+const { PORT = 3000, HOST } = process.env;
+
+const options: AppOptions = {
+  logger: true,
+};
+
+const start = async () => {
+  const app = await buildApp(options);
+
+  try {
+    await app.listen({
+      port: +PORT,
+      host: HOST,
+    });
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
